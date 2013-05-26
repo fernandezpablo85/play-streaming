17ae0a5c3edbae7c0026e3f5b894ba935995c777
Pablo Fernandez <fernandezpablo85@gmail.com>
2013-05-26 11:09:56 -0300

    add server sent events

--- app/Global.scala
+++ app/Global.scala
@@ -5,18 +5,19 @@ import play.api.Play.current
 import scala.concurrent.duration._
 import scala.util.Random
 import model.StatusUpdate
-import controllers.{Application => App}
 
 object Global extends GlobalSettings {
 
   val statuses = Array(
     StatusUpdate("pablo", "Are stream updates cool or what?"),
-    StatusUpdate("hn_commenter", "Nice tutorial!"),
-    StatusUpdate("caesar", "lorem ipsum bla bla bla bla."),
+    StatusUpdate("hn_comments", "Nice tutorial!"),
+    StatusUpdate("fake_profile", "lorem ipsum bla bla bla bla."),
     StatusUpdate("funes_mori", "please kill me before next match.")
   )
 
   override def onStart(app: Application) {
+    import controllers.{Application => App}
+
     Akka.system.scheduler.schedule(0.seconds, 50.milliseconds) {
       App.channel.push(statuses(Random.nextInt(statuses.size)))
     }    
--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -15,11 +15,21 @@ object Application extends Controller {
 
   val json = Enumeratee.map[StatusUpdate] { status => Json.toJson(status)}
   val readable = Enumeratee.map[JsValue] { obj => Json.stringify(obj) + "\n" }
+  val severSentEvents = Enumeratee.map[JsValue] {obj => "data: " + Json.stringify(obj) + "\n\n"}
   
   def index = Action {
-    Ok("the firehose is live on /firehose")
+    Ok("the firehose is live on /firehose, the SSE stream is on /events")
   }
 
+  def events = Action {
+    Ok(views.html.Sse())
+  }
+
+  def sse = Action {
+    val headers = Map("Content-Length" -> "-1", "Content-Type" -> "text/event-stream")
+    val header = ResponseHeader(200, headers)
+    SimpleResult(header, stream &> json &> severSentEvents)
+  }
 
   def firehose = Action {
     val headers = Map("Content-Length" -> "-1", "Content-Type" -> "application/json")
